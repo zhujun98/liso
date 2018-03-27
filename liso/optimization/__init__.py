1523cef01adf299ee88305ac54d628d703df581f
Jun Zhu <zhujun981661@gmail.com>
2018-03-27 15:09:01 +0200

    Not raise if pyOpt is not installed.

--- liso/optimization/__init__.py
+++ liso/optimization/__init__.py
@@ -1,12 +1,16 @@
 from .linac_optimization import LinacOptimization
-from .pyopt_linac_optimization import PyoptLinacOptimization
 from .descriptive_parameter import DescriptiveParameter
 from .covariable import Covariable
 
 
 __all__ = [
     'LinacOptimization',
-    'PyoptLinacOptimization',
     'DescriptiveParameter',
     'Covariable'
 ]
+
+try:
+    from .pyopt_linac_optimization import PyoptLinacOptimization
+    __all__.append('PyoptLinacOptimization')
+except ModuleNotFoundError:
+    pass
