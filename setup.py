eb59aa55a852d0932125f303349543db2cff0ef0
zhujun98 <zhujun981661@gmail.com>
2020-10-05 09:38:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
