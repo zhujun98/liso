499d6943b6d0e0c624f73dc45f72ab302c6103ea
zhujun98 <zhujun981661@gmail.com>
2020-10-19 15:22:21 +0200

    Remove normalization of jitter

--- liso/ml/pre_proc.py
+++ liso/ml/pre_proc.py
@@ -10,15 +10,13 @@ Copyright (C) Jun Zhu. All rights reserved.
 class Normalizer:
     """Normalize and unnormalize data and track the record."""
 
-    def __init__(self, data, *, minmax=None, jitter=None):
+    def __init__(self, data, *, minmax=None):
         """Initialization.
 
         :param pandas.DataFrame/dict data: reference data to be normalized.
             It should support __getitem__ or __setitem__.
         :param dict minmax: a dictionary of (lower boundary, higher boundary)
             of the MinMax normalizer.
-        :param dict jitter: a dictionary of (center, sigma, multiplier)
-            of the jitter normalizer.
         """
         self._minmax = dict()
         if minmax is not None:
@@ -29,27 +27,10 @@ class Normalizer:
                         f"{len(item)}!")
                 self._minmax[key] = tuple(item)
 
-        self._jitter = dict()
-        if jitter is not None:
-            for key, item in jitter.items():
-                if key in self._minmax:
-                    raise KeyError(
-                        f"Duplicated key in both MinMax and jitter normalizer:"
-                        f" {key}")
-                if len(item) != 3:
-                    raise ValueError(
-                        f"Jitter normalize must only have three parameters: "
-                        f"{len(item)}!")
-                self._jitter[key] = tuple(item)
-
         for key in data:
-            if key in self._minmax:
-                self._normalize_minmax(data, key)
-            elif key in self._jitter:
-                self._normalize_jitter(data, key)
-            else:
+            if key not in self._minmax:
                 self._minmax[key] = data[key].min(), data[key].max()
-                self._normalize_minmax(data, key)
+            self._normalize_minmax(data, key)
 
     def normalize(self, data):
         """Normalize the data inplace.
@@ -58,13 +39,11 @@ class Normalizer:
             It should support __getitem__ or __setitem__.
         """
         for key in data:
-            if key not in self._minmax and key not in self._jitter:
+            if key not in self._minmax:
                 raise KeyError(f"{key} is not found in the data!")
 
             if key in self._minmax:
                 self._normalize_minmax(data, key)
-            elif key in self._jitter:
-                self._normalize_jitter(data, key)
 
     def unnormalize(self, data):
         """Unnormalize the data inplace.
@@ -73,26 +52,16 @@ class Normalizer:
             It should support __getitem__ or __setitem__.
         """
         for key in data:
-            if key not in self._minmax and key not in self._jitter:
+            if key not in self._minmax:
                 raise KeyError(f"{key} is not found in the data!")
 
             if key in self._minmax:
                 self._unnormalize_minmax(data, key)
-            elif key in self._jitter:
-                self._unnormalize_jitter(data, key)
 
     def _normalize_minmax(self, data, key):
         lb, ub = self._minmax[key]
         data[key] = 2 * (data[key] - lb) / (ub - lb) - 1
 
-    def _normalize_jitter(self, data, key):
-        mu, sigma, multiple = self._jitter[key]
-        data[key] = (data[key] - mu) / (multiple * sigma)
-
     def _unnormalize_minmax(self, data, key):
         lb, ub = self._minmax[key]
         data[key] = 0.5 * (data[key] + 1) * (ub - lb) + lb
-
-    def _unnormalize_jitter(self, data, key):
-        mu, sigma, multiple = self._jitter[key]
-        data[key] = data[key] * multiple * sigma + mu
