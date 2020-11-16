1c0531e449441a936500be353b0d27d38381a000
zhujun <jun.zhu@desy.de>
2020-11-16 20:16:24 +0100

    Allow explicitly mark no-event-based channel

--- examples/xfel_experiment/phase_scan.py
+++ examples/xfel_experiment/phase_scan.py
@@ -17,23 +17,23 @@ m.add_control_channel(dc.FLOAT, 'XFEL.MAGNETS/MAGNET.ML/QI.64.I1D/KICK_MRAD.SP')
 
 m.add_diagnostic_channel(dc.FLOAT, 'XFEL.DIAG/CHARGE.ML/TORA.25.I1/CHARGE.ALL')
 
-# m.add_diagnostic_channel(dc.IMAGE, 'XFEL.DIAG/CAMERA/OTRC.64.I1D/IMAGE_EXT_ZMQ',
-#                          shape=(1750, 2330), dtype='uint16')
+m.add_diagnostic_channel(dc.IMAGE, 'XFEL.DIAG/CAMERA/OTRC.64.I1D/IMAGE_EXT_ZMQ',
+                         shape=(1750, 2330), dtype='uint16', no_event=True)
 
 sc = MachineScan(m)
 
 # Uncomment if you have write authority. Be careful!!!
-sc.add_param('XFEL.RF/LLRF.CONTROLLER/CTRL.GUN.I1/SP.PHASE',
-             readout='XFEL.RF/LLRF.CONTROLLER/VS.GUN.I1/PHASE.SAMPLE',
-             tol=0.02,
-             lb=-3, ub=3)
-sc.add_param('XFEL.RF/LLRF.CONTROLLER/CTRL.A1.I1/SP.PHASE',
-             readout='XFEL.RF/LLRF.CONTROLLER/VS.A1.I1/PHASE.SAMPLE',
-             tol=0.02,
-             lb=-3, ub=3)
-sc.add_param('XFEL.RF/LLRF.CONTROLLER/CTRL.AH1.I1/SP.PHASE',
-             readout='XFEL.RF/LLRF.CONTROLLER/VS.AH1.I1/PHASE.SAMPLE',
-             tol=0.02,
-             lb=182, ub=186)
+# sc.add_param('XFEL.RF/LLRF.CONTROLLER/CTRL.GUN.I1/SP.PHASE',
+#              readout='XFEL.RF/LLRF.CONTROLLER/VS.GUN.I1/PHASE.SAMPLE',
+#              tol=0.02,
+#              lb=-3, ub=3)
+# sc.add_param('XFEL.RF/LLRF.CONTROLLER/CTRL.A1.I1/SP.PHASE',
+#              readout='XFEL.RF/LLRF.CONTROLLER/VS.A1.I1/PHASE.SAMPLE',
+#              tol=0.02,
+#              lb=-3, ub=3)
+# sc.add_param('XFEL.RF/LLRF.CONTROLLER/CTRL.AH1.I1/SP.PHASE',
+#              readout='XFEL.RF/LLRF.CONTROLLER/VS.AH1.I1/PHASE.SAMPLE',
+#              tol=0.02,
+#              lb=182, ub=186)
 
-sc.scan(1230, folder='my_exp_data', n_tasks=8)
+sc.scan(1230, folder='my_exp_data', n_tasks=16)
