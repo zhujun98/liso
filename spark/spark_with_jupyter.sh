298805b223c4920ff4d1ff58dd39ac052000e1d5
Jun Zhu <zhujun981661@gmail.com>
2021-04-07 17:02:00 +0200

    Add instruction for using Jupyter notebook with Spark

--- spark/spark_with_jupyter.sh
+++ spark/spark_with_jupyter.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=all
-#SBATCH --job-name=spark-pi      # create a short name for your job
-#SBATCH --nodes=2                # node count
-#SBATCH --ntasks-per-node=16     # number of tasks per node
+#SBATCH --job-name=spark-jupyter # create a short name for your job
+#SBATCH --nodes=3                # node count
+#SBATCH --ntasks-per-node=20     # number of tasks per node
 #SBATCH --cpus-per-task=2        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem 256G               # real memory required per node
-#SBATCH --time=00:05:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=08:00:00          # total run time limit (HH:MM:SS)
 unset LD_PRELOAD
 
 TEC=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE * $SLURM_CPUS_PER_TASK))
@@ -13,13 +13,15 @@ EM=$(($SLURM_MEM_PER_NODE / $SLURM_NTASKS_PER_NODE / 1024))
 
 source /etc/profile.d/modules.sh
 module load maxwell spark/3.0.2
+# The Python version used to launch the cluster and to run pyspark must be consistent
+conda activate liso
 
 export SPARK_MASTER_URL=spark://$(hostname -f):7077
 export SPARK_CONF_DIR=$PWD/conf
 export SPARK_LOG_DIR=$PWD/logs
 export SPARK_WORKER_DIR=$PWD/logs
 
-../bin/spark_start
+bin/spark_start
 echo ${SPARK_MASTER_URL} | tee master.txt
 
-spark-submit --total-executor-cores ${TEC} --executor-memory ${EM}G --master ${SPARK_MASTER_URL} pi.py
+sleep infinity
